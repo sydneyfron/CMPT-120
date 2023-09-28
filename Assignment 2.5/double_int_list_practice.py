e64e7f82a184e0a30e50c677eb8ca6b5099680e2
sydneyfron <sydneyfron7@gmail.com>
2023-09-27 22:20:50 -0400

    ha

--- Assignment 2.5/double_int_list_practice.py
+++ Assignment 2.5/double_int_list_practice.py
@@ -1,29 +1,39 @@
 def main():
   
   #set this to any double
-  doubleValue =
+  doubleV =4.2
   
   #set this to any int
-  intValue = 
+  intV = 2
   
   #print out addition, subtraction, multiplication, and division of these two values
-  
+  print(doubleV+intV)
+  print(doubleV-intV)
+  print(doubleV*intV)
+  print(doubleV/intV)
+
   
   
   #populate this list
-  myFriends = []
+  myFriends = ["Sam", "Angelee", "Paige", "Jules", "mina"]
   
   #print out your friends at index 2 and index 3
-  
-  
+  print(myFriends[2])
+  print(myFriends[3])
+
   #populate this list with five numbers
-  fiveNumbers = []
+  list = [1,2,3,4,5]
   
   #do each of the four equations with different numbers each time.
-  
+  #not sure if this is what u meant
+  print(list[0]+list[1])
+  print(list[1]-list[2])
+  print(list[2]*list[3])
+  print(list[3]/list[4])
   #now replace two of the numbers in the list with a different number (using name of list[x] = ?, not rewriting the fiveNumber list)
-  
+  list[0]=88
+  list[1]=99
   #print out the list
-  
+  print(list)
   
 main()
