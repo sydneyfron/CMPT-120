bc40d1bea53782ddb2131b2d6485a3c3c0890044
sydneyfron <sydneyfron7@gmail.com>
2023-10-03 08:21:10 -0400

    b

--- Assignment 3/if_else_loop_practice.py
+++ Assignment 3/if_else_loop_practice.py
@@ -11,6 +11,15 @@ else (the number is less than 5)
 '''
 
 def main():
-    
+    num=[33,1,42,13,9,48,22,27]
+    for x in num:
+        if x>35:
+            print("greater than 35")
+        elif x>=20:
+            print("between 20-35")
+        elif x>=5:
+            print("between 5 and 20")
+        else:
+            print("the number is less than 5")
     
 main()
